bcd42734d64751c4eafcc07955c7a25474e85b36
Strikerzzzz <strikezz2003@gmaill.com>
2024-11-05 20:51:49 +0700

    tao form va quan ly

---
+++ BaiTapLon/QuanLy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLon
+{
+    public partial class QuanLy : Form
+    {
+        public QuanLy()
+        {
+            InitializeComponent();
+        }
+
+        private void quảnLýSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLSinhVien frm = new QLSinhVien();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýLớpHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLLopHoc frm = new QLLopHoc();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýMônHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLMonHoc frm = new QLMonHoc();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýĐiểmDanhToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLDiemDanh frm = new QLDiemDanh();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýĐiểmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLDiem frm = new QLDiem();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýLoạiĐiểmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLLoaiDiem frm = new QLLoaiDiem();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void quảnLýLớpSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QLLopSinhVien frm = new QLLopSinhVien();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
