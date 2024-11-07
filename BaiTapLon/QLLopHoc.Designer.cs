affcf47640c3ff56a51e4e0379e9b1aff9021225
Strikerzzzz <strikezz2003@gmaill.com>
2024-11-07 09:57:57 +0700

    btn thoat lop

--- BaiTapLon/QLLopHoc.Designer.cs
+++ BaiTapLon/QLLopHoc.Designer.cs
@@ -219,7 +219,7 @@
             this.btnThoat.TabIndex = 6;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
-            this.btnThoat.Click += new System.EventHandler(this.button4_Click);
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // QLLopHoc
             // 
