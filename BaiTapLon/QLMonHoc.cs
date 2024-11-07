87011300c31ddd16e6cc63651dd827f72194323f
Strikerzzzz <strikezz2003@gmaill.com>
2024-11-07 12:57:05 +0700

    update and deleted

--- BaiTapLon/QLMonHoc.cs
+++ BaiTapLon/QLMonHoc.cs
@@ -26,7 +26,7 @@ namespace BaiTapLon
         {
             try
             {
-                this.dataGridViewQLMonHoc.DataSource = DataBase.GetData("SELECT * FROM MonHoc");
+                this.dataGridViewQLMonHoc.DataSource = DataBase.GetData("SELECT * FROM MonHoc where TrangThai = 'Initialize'");
             }
             catch (Exception ex)
             {
@@ -52,51 +52,54 @@ namespace BaiTapLon
             bool result = new DataBase().UpdateData(query, parameters);
             if (result)
             {
-                MessageBox.Show("Thêm nhân viên thành công!");
+                MessageBox.Show("Thêm môn học thành công!");
                 LoadDatabase();
             }
             else
             {
-                MessageBox.Show("Thêm nhân viên thất bại.");
+                MessageBox.Show("Thêm môn học thất bại.");
             }
         }
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-            //if (lsvNhanVien.SelectedItems.Count > 0)
-            //{
-            //    ListViewItem selectedItem = lsvNhanVien.SelectedItems[0];
-            //    DataTable dt = DataBase.GetData("SELECT * FROM nhanvien");
-            //    int maNhanVien = Convert.ToInt32(dt.Rows[selectedItem.Index]["MaNhanVien"]);
-
-            //    string query = "UPDATE nhanvien SET Hotennhanvien = @Hotennhanvien, NgaySinh = @NgaySinh, DiaChi = @DiaChi, DienThoai = @DienThoai WHERE MaNhanVien = @MaNhanVien";
-            //    SqlParameter[] parameters = {
-            //        new SqlParameter("@Hotennhanvien", txtHoTen.Text),
-            //        new SqlParameter("@NgaySinh", dateTimePickerNgaySinh.Value),
-            //        new SqlParameter("@DiaChi", txtDiaChi.Text),
-            //        new SqlParameter("@DienThoai", txtDienThoai.Text),
-            //        new SqlParameter("@MaNhanVien", maNhanVien)
-            //    };
-
-            //    bool result = new DataBase().UpdateData(query, parameters);
-            //    if (result)
-            //    {
-            //        MessageBox.Show("Cập nhật nhân viên thành công!");
-            //        LoadListView();
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Cập nhật nhân viên thất bại.");
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Vui lòng chọn nhân viên để sửa.");
-            //}
+            string query = "UPDATE  MonHoc SET TenMon = @TenMon, SoTinChi = @SoTinChi, LoaiMon = @LoaiMon, TongSoBuoiHoc = @TongSoBuoiHoc WHERE MaMon = @MaMon";
+            SqlParameter[] parameters = {
+                new SqlParameter("@TenMon", txtTenMon.Text),
+                new SqlParameter("@SoTinChi", txtSoTinChi.Text),
+                new SqlParameter("@LoaiMon", txtLoaiMon.Text),
+                new SqlParameter("@TongSoBuoiHoc", txtTongSoBuoiHoc.Text),
+                new SqlParameter("@MaMon", txtMaMon.Text),
+            };
+            bool result = new DataBase().UpdateData(query, parameters);
+            if (result)
+            {
+                MessageBox.Show("Sửa môn học thành công!");
+                LoadDatabase();
+            }
+            else
+            {
+                MessageBox.Show("Sửa môn học thất bại.");
+            }
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
+            string query = "UPDATE MonHoc SET TrangThai = @TrangThai WHERE MaMon = @MaMon";
+            SqlParameter[] parameters = {
+                new SqlParameter("@MaMon", txtMaMon.Text),
+                new SqlParameter("@TrangThai", "Deleted"),
+            };
+            bool result = new DataBase().UpdateData(query, parameters);
+            if (result)
+            {
+                MessageBox.Show("Xóa môn học thành công!");
+                LoadDatabase();
+            }
+            else
+            {
+                MessageBox.Show("Xóa môn học thất bại.");
+            }
             //if (lsvNhanVien.SelectedItems.Count > 0 && DialogResult.Yes == MessageBox.Show("Bạn có chắc là muốn xóa?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
             //{
             //    ListViewItem selectedItem = lsvNhanVien.SelectedItems[0];
