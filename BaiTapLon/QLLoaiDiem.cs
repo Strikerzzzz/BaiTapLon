df85a48b78b11f3942fa9ddd8f1bb0056227e048
Strikerzzzz <strikezz2003@gmail.com>
2024-11-09 15:52:10 +0700

    LoaiDiem Finish

--- BaiTapLon/QLLoaiDiem.cs
+++ BaiTapLon/QLLoaiDiem.cs
@@ -13,8 +13,6 @@ namespace BaiTapLon
 {
     public partial class QLLoaiDiem : Form
     {
-        Dictionary<string, string> dictLM = new Dictionary<string, string>();
-
         public QLLoaiDiem()
         {
             InitializeComponent();
@@ -26,45 +24,18 @@ namespace BaiTapLon
                 MessageBox.Show("Vui lòng nhập tên loại điểm.");
                 return false;
             }
-            if (string.IsNullOrEmpty(txtTiLe.Text) || !txtTiLe.Text.All(char.IsDigit))
+            if (string.IsNullOrEmpty(txtTiLe.Text) || !double.TryParse(txtTiLe.Text, out _))
             {
-                MessageBox.Show("Vui lòng nhập tỉ lệs.");
+                MessageBox.Show("Vui lòng nhập tỉ lệ.");
                 return false;
             }
             return true;
-        }
-        void LoadListDB(DataTable dt, Dictionary<string, string> dict)
-        {
-            try
-            {
-                if (dt != null && dt.Rows.Count > 0)
-                {
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        string ma = row[0].ToString();
-                        string ten = row[1].ToString();
-
-                        if (!dict.ContainsKey(ma))
-                        {
-                            dict.Add(ma, ten);
-                        }
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Dữ liệu không tồn tại hoặc rỗng.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Lỗi khi tải danh sách: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
         void LoadDatabase()
         {
             try
             {
-                this.dataGridView1.DataSource = DataBase.GetData("SELECT IDLoaiDiem, mh.TenMon, TenLoaiDiem,Concat(TiLe,'%') as Tile FROM LoaiDiem ld LEFT JOIN MonHoc mh on ld.MaMon= mh.MaMon where ld.TrangThai = 'Initialize'");
+                this.dataGridView1.DataSource = DataBase.GetData("SELECT IDLoaiDiem, TenLoaiDiem, Tile FROM LoaiDiem where TrangThai = 'Initialize'");
             }
             catch (Exception ex)
             {
@@ -74,19 +45,6 @@ namespace BaiTapLon
         private void QLLoaiDiem_Load(object sender, EventArgs e)
         {
             LoadDatabase();
-            DataTable dt = DataBase.GetData("SELECT MaMon, TenMon FROM MonHoc where TrangThai = 'Initialize'");
-            if (dt != null && dt.Rows.Count > 0)
-            {
-                LoadListDB(dt, dictLM);
-            }
-            else
-            {
-                MessageBox.Show("Không có dữ liệu loại môn.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-            cbTenMon.DataSource = new BindingSource(dictLM, null);
-            cbTenMon.DisplayMember = "Value";
-            cbTenMon.ValueMember = "Key";
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
@@ -102,9 +60,8 @@ namespace BaiTapLon
             }
             try
             {
-                string query = "INSERT INTO LoaiDiem (MaMon, TenLoaiDiem, TiLe, TrangThai) VALUES (@MaMon, @TenLoaiDiem, @TiLe, 'Initialize')";
+                string query = "INSERT INTO LoaiDiem ( TenLoaiDiem, TiLe, TrangThai) VALUES ( @TenLoaiDiem, @TiLe, 'Initialize')";
                 SqlParameter[] parameters = {
-                    new SqlParameter("@MaMon", cbTenMon.SelectedValue?.ToString()),
                     new SqlParameter("@TenLoaiDiem", txtTenLoaiDiem.Text),
                     new SqlParameter("@TiLe", txtTiLe.Text),
                 };
@@ -133,7 +90,37 @@ namespace BaiTapLon
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-
+            if (!Valid())
+            {
+                return;
+            }
+            try
+            {
+                string query = "UPDATE  LoaiDiem SET TenLoaiDiem = @TenLoaiDiem, TiLe = @Tile WHERE IDLoaiDiem = @IDLoaiDiem";
+                SqlParameter[] parameters = {
+                    new SqlParameter("@TenLoaiDiem", txtTenLoaiDiem.Text),
+                    new SqlParameter("@TiLe", txtTiLe.Text),
+                    new SqlParameter("@IDLoaiDiem", txtID.Text),
+                };
+                bool result = new DataBase().UpdateData(query, parameters);
+                if (result)
+                {
+                    MessageBox.Show("Sửa môn học thành công!");
+                    LoadDatabase();
+                }
+                else
+                {
+                    MessageBox.Show("Sửa môn học thất bại.");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Lỗi cơ sở dữ liệu: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
@@ -172,19 +159,8 @@ namespace BaiTapLon
                 if (dataGridView1.CurrentRow != null)
                 {
                     txtID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
-                    string TenMon = dataGridView1.CurrentRow.Cells[1].Value?.ToString();
-                    string ID = dictLM.FirstOrDefault(x => x.Value == TenMon).Key;
-
-                    if (!string.IsNullOrEmpty(ID))
-                    {
-                        cbTenMon.SelectedValue = ID;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Không tìm thấy mã môn cho tên: " + ID);
-                    }
-                    txtTenLoaiDiem.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
-                    txtTiLe.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                    txtTenLoaiDiem.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                    txtTiLe.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
                 }
 
             }
