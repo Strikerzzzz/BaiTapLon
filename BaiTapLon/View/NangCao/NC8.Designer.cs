824fe951676ff80e76426f98d160bade2c663f1e
Strikerzzzz <strikezz2003@gmaill.com>
2024-11-19 20:54:29 +0700

    nang cao 8

--- BaiTapLon/View/NangCao/NC8.Designer.cs
+++ BaiTapLon/View/NangCao/NC8.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Lựa chọn";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(293, 58);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(265, 26);
+            this.comboBox.TabIndex = 42;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(564, 52);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(87, 42);
+            this.btnThoat.TabIndex = 41;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(277, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 29);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "THỐNG KÊ SINH VIÊN TRƯỢT";
+            // 
+            // chart
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart.Legends.Add(legend5);
+            this.chart.Location = new System.Drawing.Point(12, 187);
+            this.chart.Name = "chart";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart.Series.Add(series5);
+            this.chart.Size = new System.Drawing.Size(876, 309);
+            this.chart.TabIndex = 39;
+            this.chart.Text = "chart1";
             // 
             // NC8
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 506);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.Name = "NC8";
             this.Text = "NC8";
+            this.Load += new System.EventHandler(this.NC8_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
--- BaiTapLon/View/NangCao/NC8.cs
+++ BaiTapLon/View/NangCao/NC8.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace BaiTapLon.View.NangCao
 {
@@ -15,6 +16,334 @@ namespace BaiTapLon.View.NangCao
         public NC8()
         {
             InitializeComponent();
+        }
+
+        private void NC8_Load(object sender, EventArgs e)
+        {
+            comboBox.Items.Add("Theo môn");
+            comboBox.Items.Add("Theo kỳ");
+            comboBox.Items.Add("Theo ngành");
+            comboBox.Items.Add("Theo lớp");
+            comboBox.SelectedIndex = 0;
+            LoadChartData(GetQueryForSelectedComboBox());
+        }
+        private string GetQueryForSelectedComboBox()
+        {
+            if (comboBox.SelectedItem.ToString() == "Theo môn")
+            {
+                return @"WITH DiemTrungBinh AS (
+                    SELECT 
+                        d.MaSV,
+                        d.MaMon,
+                        mh.TenMon,
+                        SUM(d.GiaTriDiem * (ld.TiLe / 100)) AS DiemTrungBinh
+                    FROM 
+                        Diem d
+                    INNER JOIN 
+                        LoaiDiem ld ON d.IDLoaiDiem = ld.IDLoaiDiem
+                    INNER JOIN 
+                        MonHoc mh ON d.MaMon = mh.MaMon
+                    WHERE 
+                        d.TrangThai = 'Initialize'
+                    GROUP BY 
+                        d.MaSV, d.MaMon, mh.TenMon
+                ),
+                KetQua AS (
+                    SELECT 
+                        TenMon,
+                        CASE 
+                            WHEN DiemTrungBinh >= 4.0 THEN 1
+                            ELSE 0
+                        END AS Pass,
+                        CASE 
+                            WHEN DiemTrungBinh < 4.0 THEN 1
+                            ELSE 0
+                        END AS Fail
+                    FROM 
+                        DiemTrungBinh
+                )
+                SELECT 
+                    TenMon,
+                    SUM(Pass) AS SoLuongPass,
+                    SUM(Fail) AS SoLuongFail
+                FROM 
+                    KetQua
+                GROUP BY 
+                    TenMon
+                ORDER BY 
+                    TenMon;";
+            }
+            else if (comboBox.SelectedItem.ToString() == "Theo kỳ")
+            {
+                return @"WITH DiemTrungBinh AS (
+                    SELECT 
+                        d.MaSV,
+                        CONCAT(h.TenHocKy, ' - ', h.Nam) AS HocKyNam, -- Gộp tên học kỳ và năm học
+                        SUM(d.GiaTriDiem * (ld.TiLe / 100)) AS DiemTrungBinh
+                    FROM 
+                        Diem d
+                    INNER JOIN 
+                        LoaiDiem ld ON d.IDLoaiDiem = ld.IDLoaiDiem
+                    INNER JOIN 
+                        SinhVien sv ON d.MaSV = sv.MaSV
+                    INNER JOIN 
+                        LopHoc_SinhVien lhs ON sv.MaSV = lhs.MaSV
+                    INNER JOIN 
+                        LopHoc lh ON lhs.MaLop = lh.MaLop
+                    INNER JOIN 
+                        HocKy h ON lh.IDHocKy = h.IDHocKy -- Kết nối với bảng HocKy
+                    WHERE 
+                        d.TrangThai = 'Initialize' -- Điều kiện trạng thái điểm
+                    GROUP BY 
+                        d.MaSV, h.TenHocKy, h.Nam
+                ),
+                KetQua AS (
+                    SELECT 
+                        HocKyNam,
+                        CASE 
+                            WHEN DiemTrungBinh >= 4.0 THEN 1
+                            ELSE 0
+                        END AS Pass,
+                        CASE 
+                            WHEN DiemTrungBinh < 4.0 THEN 1
+                            ELSE 0
+                        END AS Fail
+                    FROM 
+                        DiemTrungBinh
+                )
+                SELECT 
+                    HocKyNam,
+                    SUM(Pass) AS SoLuongPass,
+                    SUM(Fail) AS SoLuongFail
+                FROM 
+                    KetQua
+                GROUP BY 
+                    HocKyNam
+                ORDER BY 
+                    HocKyNam;";
+            }
+            else if (comboBox.SelectedItem.ToString() == "Theo ngành")
+            {
+                return @"WITH DiemTrungBinhNganh AS (
+                    SELECT 
+                        d.MaSV,
+                        sv.MaChuyenNganh,
+                        cn.TenChuyenNganh,
+                        SUM(d.GiaTriDiem * (ld.TiLe / 100)) AS DiemTrungBinh
+                    FROM 
+                        Diem d
+                    INNER JOIN 
+                        LoaiDiem ld ON d.IDLoaiDiem = ld.IDLoaiDiem
+                    INNER JOIN 
+                        SinhVien sv ON d.MaSV = sv.MaSV
+                    INNER JOIN 
+                        ChuyenNganh cn ON sv.MaChuyenNganh = cn.MaChuyenNganh
+                    WHERE 
+                        d.TrangThai = 'Initialize'
+                    GROUP BY 
+                        d.MaSV, sv.MaChuyenNganh, cn.TenChuyenNganh
+                ),
+                KetQuaNganh AS (
+                    SELECT 
+                        TenChuyenNganh,
+                        CASE 
+                            WHEN DiemTrungBinh >= 4.0 THEN 1
+                            ELSE 0
+                        END AS Pass,
+                        CASE 
+                            WHEN DiemTrungBinh < 4.0 THEN 1
+                            ELSE 0
+                        END AS Fail
+                    FROM 
+                        DiemTrungBinhNganh
+                )
+                SELECT 
+                    TenChuyenNganh,
+                    SUM(Pass) AS SoLuongPass,
+                    SUM(Fail) AS SoLuongFail
+                FROM 
+                    KetQuaNganh
+                GROUP BY 
+                    TenChuyenNganh
+                ORDER BY 
+                    TenChuyenNganh;";
+            }
+            else if (comboBox.SelectedItem.ToString() == "Theo lớp")
+            {
+                return @"WITH DiemTrungBinh AS (
+                    SELECT 
+                        d.MaSV,
+                        lh.MaLop,
+                        lh.TenLop,
+                        SUM(d.GiaTriDiem * (ld.TiLe / 100)) AS DiemTrungBinh
+                    FROM 
+                        Diem d
+                    INNER JOIN 
+                        LoaiDiem ld ON d.IDLoaiDiem = ld.IDLoaiDiem
+                    INNER JOIN 
+                        SinhVien sv ON d.MaSV = sv.MaSV
+                    INNER JOIN 
+                        LopHoc_SinhVien lhs ON sv.MaSV = lhs.MaSV
+                    INNER JOIN 
+                        LopHoc lh ON lhs.MaLop = lh.MaLop
+                    WHERE 
+                        d.TrangThai = 'Initialize'
+                    GROUP BY 
+                        d.MaSV, lh.MaLop, lh.TenLop
+                ),
+                KetQua AS (
+                    SELECT 
+                        TenLop,
+                        CASE 
+                            WHEN DiemTrungBinh >= 4.0 THEN 1
+                            ELSE 0
+                        END AS Pass,
+                        CASE 
+                            WHEN DiemTrungBinh < 4.0 THEN 1
+                            ELSE 0
+                        END AS Fail
+                    FROM 
+                        DiemTrungBinh
+                )
+                SELECT 
+                    TenLop,
+                    SUM(Pass) AS SoLuongPass,
+                    SUM(Fail) AS SoLuongFail
+                FROM 
+                    KetQua
+                GROUP BY 
+                    TenLop
+                ORDER BY 
+                    TenLop;";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private void LoadChartData(string query)
+        {
+            try
+            {
+                // Lấy dữ liệu từ cơ sở dữ liệu
+                DataTable dt = DataBase.GetData(query);
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    // Xóa dữ liệu cũ trên Chart
+                    chart.Series.Clear();
+
+                    // Tắt lưới ngang (trục Y) và lưới dọc (trục X)
+                    chart.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+                    chart.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+                    chart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+                    chart.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+
+                    // Tạo Series mới cho biểu đồ
+                    Series seriesPass = new Series("Pass");
+                    Series seriesFail = new Series("Fail");
+
+                    seriesPass.ChartType = SeriesChartType.Column;
+                    seriesFail.ChartType = SeriesChartType.Column;
+
+                    seriesPass.IsValueShownAsLabel = true; // Hiển thị giá trị trên cột
+                    seriesFail.IsValueShownAsLabel = true;
+
+                    // Phân loại dữ liệu dựa trên cột có trong bảng
+                    string columnX = string.Empty;
+                    string columnPass = string.Empty;
+                    string columnFail = string.Empty;
+
+                    if (dt.Columns.Contains("TenMon"))
+                    {
+                        columnX = "TenMon";
+                        columnPass = "SoLuongPass";
+                        columnFail = "SoLuongFail";
+                    }
+                    else if (dt.Columns.Contains("HocKyNam"))
+                    {
+                        columnX = "HocKyNam";
+                        columnPass = "SoLuongPass";
+                        columnFail = "SoLuongFail";
+                    }
+                    else if (dt.Columns.Contains("TenChuyenNganh"))
+                    {
+                        columnX = "TenChuyenNganh";
+                        columnPass = "SoLuongPass";
+                        columnFail = "SoLuongFail";
+                    }
+                    else if (dt.Columns.Contains("TenLop"))
+                    {
+                        columnX = "TenLop";
+                        columnPass = "SoLuongPass";
+                        columnFail = "SoLuongFail";
+                    }
+
+                    // Thêm dữ liệu vào các Series
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string xValue = row[columnX].ToString();
+                        int passValue = Convert.ToInt32(row[columnPass]);
+                        int failValue = Convert.ToInt32(row[columnFail]);
+
+                        seriesPass.Points.AddXY(xValue, passValue);
+                        seriesFail.Points.AddXY(xValue, failValue);
+                    }
+
+                    // Thêm Series vào biểu đồ
+                    chart.Series.Add(seriesPass);
+                    chart.Series.Add(seriesFail);
+
+                    // Cấu hình trục và tiêu đề biểu đồ
+                    if (columnX == "TenMon")
+                    {
+                        chart.ChartAreas[0].AxisX.Title = "Môn học";
+                    }
+                    else if (columnX == "TenHocKy")
+                    {
+                        chart.ChartAreas[0].AxisX.Title = "Kỳ học";
+                    }
+                    else if (columnX == "TenChuyenNganh")
+                    {
+                        chart.ChartAreas[0].AxisX.Title = "Ngành học";
+                    }
+                    else if (columnX == "TenLop")
+                    {
+                        chart.ChartAreas[0].AxisX.Title = "Lớp học";
+                    }
+                    else
+                    {
+                        chart.ChartAreas[0].AxisX.Title = "Danh mục";
+                    }
+
+                    chart.ChartAreas[0].AxisY.Title = "Số lượng";
+                    chart.ChartAreas[0].AxisX.Interval = 1; // Hiển thị tất cả các nhãn
+
+                    // Thêm tiêu đề cho biểu đồ
+                    chart.Titles.Clear();
+                    chart.Titles.Add($"Thống kê theo {chart.ChartAreas[0].AxisX.Title.ToLower()}");
+                }
+                else
+                {
+                    MessageBox.Show("Không có dữ liệu để hiển thị biểu đồ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu biểu đồ: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadChartData(GetQueryForSelectedComboBox());
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
