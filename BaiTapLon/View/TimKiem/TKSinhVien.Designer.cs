a772e4e285bd73fddcf88eab7f354c233c03c90a
Lethihoa26 <lthoa.dhti15a10hn@sv.uneti.edu.vn>
2024-11-20 17:50:04 +0700

    tim kiem sinh vien

--- BaiTapLon/View/TimKiem/TKSinhVien.Designer.cs
+++ BaiTapLon/View/TimKiem/TKSinhVien.Designer.cs
@@ -28,20 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboSinhVien = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewQLSV = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTK = new System.Windows.Forms.Button();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.MaSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoDem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CCCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoDienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KhoaHoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenChuyenNganh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cboDuLieu = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQLSV)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cboSinhVien
+            // 
+            this.cboSinhVien.FormattingEnabled = true;
+            this.cboSinhVien.Location = new System.Drawing.Point(433, 75);
+            this.cboSinhVien.Name = "cboSinhVien";
+            this.cboSinhVien.Size = new System.Drawing.Size(431, 26);
+            this.cboSinhVien.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lựa chọn : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(495, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TÌM KIẾM SINH VIÊN";
+            // 
+            // dataGridViewQLSV
+            // 
+            this.dataGridViewQLSV.AllowUserToAddRows = false;
+            this.dataGridViewQLSV.AllowUserToDeleteRows = false;
+            this.dataGridViewQLSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewQLSV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaSV,
+            this.HoDem,
+            this.Ten,
+            this.Email,
+            this.CCCD,
+            this.SoDienThoai,
+            this.GioiTinh,
+            this.NgaySinh,
+            this.DiaChi,
+            this.KhoaHoc,
+            this.TenChuyenNganh});
+            this.dataGridViewQLSV.Location = new System.Drawing.Point(9, 247);
+            this.dataGridViewQLSV.Name = "dataGridViewQLSV";
+            this.dataGridViewQLSV.RowHeadersVisible = false;
+            this.dataGridViewQLSV.RowHeadersWidth = 51;
+            this.dataGridViewQLSV.RowTemplate.Height = 24;
+            this.dataGridViewQLSV.Size = new System.Drawing.Size(1277, 206);
+            this.dataGridViewQLSV.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 18);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Giá trị :";
+            // 
+            // btnTK
+            // 
+            this.btnTK.Location = new System.Drawing.Point(556, 181);
+            this.btnTK.Name = "btnTK";
+            this.btnTK.Size = new System.Drawing.Size(96, 42);
+            this.btnTK.TabIndex = 36;
+            this.btnTK.Text = "Tìm kiếm";
+            this.btnTK.UseVisualStyleBackColor = true;
+            this.btnTK.Click += new System.EventHandler(this.btnTK_Click);
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(433, 128);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(219, 24);
+            this.txtTK.TabIndex = 35;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(675, 181);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(87, 42);
+            this.btnThoat.TabIndex = 34;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // MaSV
+            // 
+            this.MaSV.DataPropertyName = "MaSV";
+            this.MaSV.HeaderText = "Mã";
+            this.MaSV.MinimumWidth = 6;
+            this.MaSV.Name = "MaSV";
+            this.MaSV.Width = 50;
+            // 
+            // HoDem
+            // 
+            this.HoDem.DataPropertyName = "HoDem";
+            this.HoDem.HeaderText = "Họ đệm";
+            this.HoDem.MinimumWidth = 6;
+            this.HoDem.Name = "HoDem";
+            this.HoDem.Width = 125;
+            // 
+            // Ten
+            // 
+            this.Ten.DataPropertyName = "Ten";
+            this.Ten.HeaderText = "Tên";
+            this.Ten.MinimumWidth = 6;
+            this.Ten.Name = "Ten";
+            this.Ten.Width = 80;
+            // 
+            // Email
+            // 
+            this.Email.DataPropertyName = "Email";
+            this.Email.HeaderText = "Email";
+            this.Email.MinimumWidth = 6;
+            this.Email.Name = "Email";
+            this.Email.Width = 140;
+            // 
+            // CCCD
+            // 
+            this.CCCD.DataPropertyName = "CCCD";
+            this.CCCD.HeaderText = "Căn cước công dân";
+            this.CCCD.MinimumWidth = 6;
+            this.CCCD.Name = "CCCD";
+            this.CCCD.Width = 170;
+            // 
+            // SoDienThoai
+            // 
+            this.SoDienThoai.DataPropertyName = "SoDienThoai";
+            this.SoDienThoai.HeaderText = "Số điện thoại";
+            this.SoDienThoai.MinimumWidth = 6;
+            this.SoDienThoai.Name = "SoDienThoai";
+            this.SoDienThoai.Width = 125;
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.HeaderText = "Giới tính";
+            this.GioiTinh.MinimumWidth = 6;
+            this.GioiTinh.Name = "GioiTinh";
+            this.GioiTinh.Width = 50;
+            // 
+            // NgaySinh
+            // 
+            this.NgaySinh.DataPropertyName = "NgaySinh";
+            this.NgaySinh.HeaderText = "Ngày sinh";
+            this.NgaySinh.MinimumWidth = 6;
+            this.NgaySinh.Name = "NgaySinh";
+            this.NgaySinh.Width = 125;
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.HeaderText = "Địa chỉ";
+            this.DiaChi.MinimumWidth = 6;
+            this.DiaChi.Name = "DiaChi";
+            this.DiaChi.Width = 125;
+            // 
+            // KhoaHoc
+            // 
+            this.KhoaHoc.DataPropertyName = "KhoaHoc";
+            this.KhoaHoc.HeaderText = "Khoá học";
+            this.KhoaHoc.MinimumWidth = 6;
+            this.KhoaHoc.Name = "KhoaHoc";
+            this.KhoaHoc.Width = 110;
+            // 
+            // TenChuyenNganh
+            // 
+            this.TenChuyenNganh.DataPropertyName = "TenChuyenNganh";
+            this.TenChuyenNganh.HeaderText = "Tên chuyên ngành";
+            this.TenChuyenNganh.MinimumWidth = 6;
+            this.TenChuyenNganh.Name = "TenChuyenNganh";
+            this.TenChuyenNganh.Width = 170;
+            // 
+            // cboDuLieu
+            // 
+            this.cboDuLieu.FormattingEnabled = true;
+            this.cboDuLieu.Location = new System.Drawing.Point(675, 131);
+            this.cboDuLieu.Name = "cboDuLieu";
+            this.cboDuLieu.Size = new System.Drawing.Size(189, 26);
+            this.cboDuLieu.TabIndex = 38;
+            this.cboDuLieu.SelectedIndexChanged += new System.EventHandler(this.cboDuLieu_SelectedIndexChanged_1);
             // 
             // TKSinhVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(996, 559);
+            this.ClientSize = new System.Drawing.Size(1298, 468);
+            this.Controls.Add(this.cboDuLieu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnTK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.dataGridViewQLSV);
+            this.Controls.Add(this.cboSinhVien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.Name = "TKSinhVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TKSinhVien";
+            this.Load += new System.EventHandler(this.TKSinhVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQLSV)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboSinhVien;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewQLSV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTK;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoDem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CCCD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoDienThoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KhoaHoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenChuyenNganh;
+        private System.Windows.Forms.ComboBox cboDuLieu;
     }
 }
--- BaiTapLon/View/TimKiem/TKSinhVien.cs
+++ BaiTapLon/View/TimKiem/TKSinhVien.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace BaiTapLon.View.TimKiem
@@ -15,6 +11,163 @@ namespace BaiTapLon.View.TimKiem
         public TKSinhVien()
         {
             InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TKSinhVien_Load(object sender, EventArgs e)
+        {
+            cboSinhVien.Items.Add("Vui lòng chọn lựa chọn của bạn");
+            cboSinhVien.Items.Add("Mã sinh viên");
+            cboSinhVien.Items.Add("Họ đệm");
+            cboSinhVien.Items.Add("Tên");
+            cboSinhVien.Items.Add("Giới tính");
+            cboSinhVien.Items.Add("Địa chỉ");
+            cboSinhVien.Items.Add("Khoá học");
+            cboSinhVien.Items.Add("Tên chuyên ngành");
+            cboSinhVien.SelectedIndex = 0;
+
+            cboSinhVien.SelectedIndexChanged += cboSinhVien_SelectedIndexChanged;
+            cboDuLieu.SelectedIndexChanged += cboDuLieu_SelectedIndexChanged_1;
+
+            LoadSinhVienData();
+        }
+
+        private void LoadSinhVienData()
+        {
+            DataTable dtSinhVien = DataBase.GetData(@"SELECT MaSV, HoDem, Ten, Email, CCCD, SoDienThoai, GioiTinh, NgaySinh, DiaChi, 
+                                             cn.TenChuyenNganh, KhoaHoc 
+                                             FROM SinhVien sv 
+                                             LEFT JOIN ChuyenNganh cn ON cn.MaChuyenNganh = sv.MaChuyenNganh 
+                                             WHERE sv.TrangThai = 'Initialize' AND cn.TrangThai = 'Initialize'");
+            dataGridViewQLSV.DataSource = dtSinhVien;
+        }
+
+        private void LoadDatabaseWithParams(string sql, SqlParameter[] parameters)
+        {
+            try
+            {
+                this.dataGridViewQLSV.DataSource = DataBase.GetData(sql, parameters);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private void btnTK_Click(object sender, EventArgs e)
+        {
+            string value = txtTK.Text.Trim();
+            string columnName = cboSinhVien.SelectedItem?.ToString();
+            string selectedValue = cboDuLieu.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(columnName) || columnName == "Vui lòng chọn lựa chọn của bạn")
+            {
+                MessageBox.Show("Vui lòng chọn cột để tìm kiếm!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string selectedColumn = MapToDatabaseColumn(columnName);
+
+            if (string.IsNullOrEmpty(value) && (selectedValue == "Vui lòng chọn dữ liệu" || string.IsNullOrEmpty(selectedValue)))
+            {
+                LoadSinhVienData();
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    string query = $@"SELECT MaSV, HoDem, Ten, Email, CCCD, SoDienThoai, GioiTinh, NgaySinh, DiaChi, cn.TenChuyenNganh, KhoaHoc 
+                              FROM SinhVien sv 
+                              LEFT JOIN ChuyenNganh cn ON cn.MaChuyenNganh = sv.MaChuyenNganh 
+                              WHERE sv.TrangThai = 'Initialize' AND cn.TrangThai = 'Initialize' AND {selectedColumn} LIKE @value";
+
+                    SqlParameter[] parameters = {
+                new SqlParameter("@value", $"%{value}%")
+            };
+
+                    LoadDatabaseWithParams(query, parameters);
+                }
+            }
+        }
+
+        private void cboSinhVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string columnName = cboSinhVien.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(columnName) && columnName != "Vui lòng chọn lựa chọn của bạn")
+            {
+                DataTable data = GetDistinctDataForComboBox(columnName);
+
+                cboDuLieu.Items.Clear();
+                cboDuLieu.Items.Add("Vui lòng chọn dữ liệu");
+
+                foreach (DataRow row in data.Rows)
+                {
+                    cboDuLieu.Items.Add(row[0].ToString());
+                }
+
+                if (cboDuLieu.Items.Count > 0)
+                {
+                    cboDuLieu.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                cboDuLieu.Items.Clear();
+            }
+        }
+      
+        private DataTable GetDistinctDataForComboBox(string columnName)
+        {
+            string sql = $@"SELECT DISTINCT {MapToDatabaseColumn(columnName)} 
+                            FROM SinhVien sv 
+                            LEFT JOIN ChuyenNganh cn ON cn.MaChuyenNganh = sv.MaChuyenNganh 
+                            WHERE sv.TrangThai = 'Initialize' AND cn.TrangThai = 'Initialize'";
+            return DataBase.GetData(sql, null);
+        }
+
+        private string MapToDatabaseColumn(string displayName)
+        {
+            switch (displayName)
+            {
+                case "Mã sinh viên": return "MaSV";
+                case "Họ đệm": return "HoDem";
+                case "Tên": return "Ten";
+                case "Giới tính": return "GioiTinh";
+                case "Địa chỉ": return "DiaChi";
+                case "Khoá học": return "KhoaHoc";
+                case "Tên chuyên ngành": return "TenChuyenNganh";
+                default: throw new ArgumentException("Cột không hợp lệ!");
+            }
+
+        }
+
+        private void cboDuLieu_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string selectedColumn = cboSinhVien.SelectedItem?.ToString();
+            string selectedValue = cboDuLieu.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedColumn) && !string.IsNullOrEmpty(selectedValue))
+            {
+                string columnName = MapToDatabaseColumn(selectedColumn);
+
+                string query = $@"SELECT MaSV, HoDem, Ten, Email, CCCD, SoDienThoai, GioiTinh, NgaySinh, DiaChi, cn.TenChuyenNganh, KhoaHoc 
+                          FROM SinhVien sv 
+                          LEFT JOIN ChuyenNganh cn ON cn.MaChuyenNganh = sv.MaChuyenNganh 
+                          WHERE sv.TrangThai = 'Initialize' AND cn.TrangThai = 'Initialize' AND {columnName} = @value";
+
+                SqlParameter[] parameters = {
+                    new SqlParameter("@value", selectedValue)
+                };
+                LoadDatabaseWithParams(query, parameters);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn đầy đủ thông tin để tìm kiếm!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
